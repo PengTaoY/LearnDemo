8a5ba9877943ac10f796229cfe0cda491c0c4e7b
PengtaoY <1197726544@qq.com>
2017-07-19 16:01:46 +0800

    singalR死在半路上了

---
+++ TxSms.SingalR/Utility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TxSms.Actions;
+
+namespace TxSms.SingalR
+{
+    internal class Utility
+    {
+        public static int IntNum = 0;
+
+        /// <summary>
+        /// 时间间隔
+        /// </summary>
+        /// <returns></returns>
+        public static int TimerValue()
+        {
+            return 1000;
+        }
+
+        public static double ActivityTimerInterval()
+        {
+            return 1000.0;
+        }
+
+        /// <summary>
+        /// 获取当前UUID
+        /// </summary>
+        /// <returns></returns>
+        public static string GetUUID(string connectionId)
+        {
+            try
+            {
+                var model = new QRCodeAction().GetValidModel(connectionId);
+                return model.ToJson(connectionId);
+            }
+            catch
+            {
+                return "ERROR";
+            }
+        }
+
+        /// <summary>
+        /// 删除过期UUID
+        /// </summary>
+        public static void ClearExpiredUUID()
+        {
+            IntNum++;
+            if (IntNum <= 1000) return;
+            new QRCodeAction().ClearExpiredUUID();
+            IntNum = 0;
+        }
+    }
+}
