5194c993faa2c647e1158ff87ce5a10f4322919b
PengtaoY <1197726544@qq.com>
2018-07-09 14:29:41 +0800

    添加了敏感词过滤的方法

---
+++ 敏感词过滤2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 敏感词过滤2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilterHelper filter = new FilterHelper(@"C:\Users\Administrator\Documents\Visual Studio 2017\Projects\LearnDemo\敏感词过滤2\badword.txt");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("请输入要检测的文本:");
+                string sourceText = Console.ReadLine();
+
+                filter.SourctText = sourceText.Trim();
+
+                string resultStr = filter.Filter('*');
+
+                Console.WriteLine("替换后的内容为：");
+                Console.WriteLine(resultStr);
+
+                Console.ReadKey();
+            }
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 敏感词过滤3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace 敏感词过滤3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //敏感词词库
+            string s = "中国|国家人|zg人|64";
+            //要检测的语句
+            string test = "我是中国人，我参与了64事件";
+
+            StringSearch iwords = new StringSearch();
+            iwords.SetKeywords(s.Split('|'));
+
+            var b = iwords.ContainsAny(test);
+
+            var f = iwords.FindFirst(test);
+            ////Assert.AreEqual("中国", f);
+
+            var all = iwords.FindAll(test);
+            ////Assert.AreEqual("中国", all[0]);
+            ////Assert.AreEqual("国人", all[1]);
+            ////Assert.AreEqual(2, all.Count);
+
+            var str = iwords.Replace(test, '*');
+
+
+            Console.WriteLine($"原字符串：{test} \r\n替换过后：{str}");
+
+            Console.ReadKey();
+        }
+    }
+}
